59ee0eb65165400446c8a927bd5932d35072a8e4
torufa <torufa3@gmail.com>
2026-01-10 16:08:28 +0600

    v1

---
+++ script.js
@@ -0,0 +1,25 @@
+let textInput = document.getElementById("textInput");
+
+let wordCount = document.getElementById("wordCount");
+let charCount = document.getElementById("charCount");
+let sentenceCount = document.getElementById("sentenceCount");
+let paragraphCount = document.getElementById("paragraphCount");
+
+textInput.addEventListener("input", function () {
+   let text = textInput.value.trim();
+//Words Counting
+   let words = (text === "" ? [] : (text.split(/\s+/)));
+   wordCount.innerText = words.length;
+//character counting
+    
+
+
+
+//animation
+   let span = document.querySelectorAll("span");
+   span.forEach((e)=> {e.classList.add("beat");});
+   setTimeout(() => {
+    span.forEach((e)=>{e.classList.remove("beat");});
+   }, 1000); 
+   
+});
